f98e871dc007bae0f2cc4ed66cde3d7b7ac02145
pavel.s@smplt.net <pavel.s@smplt.net>
2021-12-23 18:46:07 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,6 @@
 
 set -e
 
-
-if [ "$NAMESPACE" != "null" ];
-then
-echo "restart in process ..."
-echo $NAMESPACE
-
-# Extract the base64 encoded config data and write this to the KUBECONFIG
 echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
 export KUBECONFIG=/tmp/config
-
 for i in $(kubectl get deploy -o wide -n $NAMESPACE| grep $IMAGE | awk {'print $1'}); do kubectl delete po -l app=$i; done
-echo $NAMESPACE
-echo $IMAGE
-else
-echo "Restart is DISABLED, namespase is null"
-fi
