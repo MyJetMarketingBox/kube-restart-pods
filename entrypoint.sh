0091ff05f93c0b1e8f507154c20bfea2d410e5d9
pavel.s@smplt.net <pavel.s@smplt.net>
2021-12-23 19:50:30 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,9 @@ export KUBECONFIG=/tmp/config
 
 for i in $(kubectl get deploy -o wide -n $NAMESPACE| grep $IMAGE | awk {'print $1'}); do kubectl delete po -l app=$i; done
 
-$(kubectl get deploy -o wide -n $NAMESPACE| grep $IMAGE | awk {'print $1'})
+#$(kubectl get deploy -o wide -n $NAMESPACE| grep $IMAGE | awk {'print $1'})
+kubectl delete po -l app=marketing-box-affiliate-service
+#kubectl get pods
 
-kubectl get pods
-
-echo $NAMESPACE
+#echo $NAMESPACE
 
