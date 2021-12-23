7dfe9fea5f7c1110df57580570c8febb67ce9936
pavel.s@smplt.net <pavel.s@smplt.net>
2021-12-23 17:28:30 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ echo $NAMESPACE
 echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
 export KUBECONFIG=/tmp/config
 
-for i in $(kubectl get deploy -o wide | grep $IMAGE | awk {'print $1'}); do kubectl delete po -l app=$i; done
+for i in $(kubectl get deploy -o wide -n $NAMESPACE| grep $IMAGE | awk {'print $1'}); do kubectl delete po -l app=$i; done
 
 else
 echo "Restart is DISABLED, namespase is null"
