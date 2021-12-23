cbd3b1c1477abf7bb2311b8d4f832b94eb596973
pavel.s@smplt.net <pavel.s@smplt.net>
2021-12-23 20:20:39 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,24 @@
 
 set -e
 
+
+if [ "$NAMESPACE" != "null" ];
+then
+echo "restart in process ..."
+echo $NAMESPACE
+
+
 echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
 export KUBECONFIG=/tmp/config
 
 for i in $(kubectl get deploy -o wide -n $NAMESPACE| grep $IMAGE | awk {'print $1'}); do kubectl delete po -l app=$i; done
 
-#$(kubectl get deploy -o wide -n $NAMESPACE| grep $IMAGE | awk {'print $1'})
-kubectl delete po -l app=marketing-box-affiliate-service -n marketing-box
-#kubectl get pods
+POD=$(kubectl get deploy -o wide -n $NAMESPACE| grep $IMAGE | awk {'print $1'})
+RN=$(kubectl describe deployment $POD -n $NAMESPACE|grep NewReplicaSet:|awk '{print $2}')
+PODS=$(for i in $(kubectl get pod -n $NAMESPACE| grep ${RN}|awk '{print $1}'); do echo $i; done)
+echo ${PODS}
+kubectl delete pod ${PODS} -n $NAMESPACE
 
-#echo $NAMESPACE
-
+else
+echo "Restart is DISABLED, namespase is null"
+fi
