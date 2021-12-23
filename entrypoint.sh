deacd7df98af4a313737bac00a7d11083eee1883
pavel.s@smplt.net <pavel.s@smplt.net>
2021-12-23 20:50:29 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
@@ -8,17 +8,15 @@ then
 echo "restart in process ..."
 echo $NAMESPACE
 
-
+# Extract the base64 encoded config data and write this to the KUBECONFIG
 echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
+NAMESPACE=marketing-box
 export KUBECONFIG=/tmp/config
 
-for i in $(kubectl get deploy -o wide -n $NAMESPACE| grep $IMAGE | awk {'print $1'}); do kubectl delete po -l app=$i; done
-
-POD=$(kubectl get deploy -o wide -n $NAMESPACE| grep $IMAGE | awk {'print $1'})
-RN=$(kubectl describe deployment $POD -n $NAMESPACE|grep NewReplicaSet:|awk '{print $2}')
-PODS=$(for i in $(kubectl get pod -n $NAMESPACE| grep ${RN}|awk '{print $1}'); do echo $i; done)
-echo ${PODS}
-kubectl delete pod ${PODS} -n $NAMESPACE
+TEST=($(for i in $(kubectl get deploy -o wide -n $NAMESPACE | grep $IMAGE | awk {'print $1'}); do echo $i; done))
+RN=($(for i in ${TEST[@]}; do kubectl describe deployment $i -n  $NAMESPACE|grep NewReplicaSet:|awk '{print $2}'; done))
+ARR=($(for i in ${RN[@]}; do kubectl get pods -n $NAMESPACE | grep $i | awk {'print $1'}; done))
+for i in ${ARR[@]}; do kubectl delete pod $i -n $NAMESPACE; done
 
 else
 echo "Restart is DISABLED, namespase is null"
