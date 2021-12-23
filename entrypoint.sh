3da3cac8c79e9fae374abff3afdd1b6c04cfec3f
pavel.s@smplt.net <pavel.s@smplt.net>
2021-12-23 19:44:54 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
 export KUBECONFIG=/tmp/config
-#kubectl get deploy -o wide -n $NAMESPACE
-echo $IMAGE
-kubectl delete po -l app=$(kubectl get deploy -o wide -n $NAMESPACE| grep $IMAGE | awk {'print $1'})
-#for i in $(kubectl get deploy -o wide -n $NAMESPACE| grep $IMAGE | awk {'print $1'}); do kubectl delete po -l app=$i; done
-for i in a b c; do echo $i; done
+
+for i in $(kubectl get deploy -o wide -n $NAMESPACE| grep $IMAGE | awk {'print $1'}); do kubectl delete po -l app=$i; done
+
+$(kubectl get deploy -o wide -n $NAMESPACE| grep $IMAGE | awk {'print $1'})
+
+kubectl get pods
+
+echo $NAMESPACE
+
