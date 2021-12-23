d0dd7d7fe0ba4cd675477202da148ad8a9cd7d22
pavel.s@smplt.net <pavel.s@smplt.net>
2021-12-23 20:56:39 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,21 +3,14 @@
 set -e
 
 
-if [ "$NAMESPACE" != "null" ];
-then
-echo "restart in process ..."
-echo $NAMESPACE
-
 # Extract the base64 encoded config data and write this to the KUBECONFIG
 echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
+IMAGE=marketingbox.integration-bridge
 NAMESPACE=marketing-box
-export KUBECONFIG=/tmp/config
+#export KUBECONFIG=/tmp/config
 
 TEST=($(for i in $(kubectl get deploy -o wide -n $NAMESPACE | grep $IMAGE | awk {'print $1'}); do echo $i; done))
 RN=($(for i in ${TEST[@]}; do kubectl describe deployment $i -n  $NAMESPACE|grep NewReplicaSet:|awk '{print $2}'; done))
 ARR=($(for i in ${RN[@]}; do kubectl get pods -n $NAMESPACE | grep $i | awk {'print $1'}; done))
 for i in ${ARR[@]}; do kubectl delete pod $i -n $NAMESPACE; done
 
-else
-echo "Restart is DISABLED, namespase is null"
-fi
